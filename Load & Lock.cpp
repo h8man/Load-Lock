f807a83e9f2a25320432719c3c56dc3e9b502ce1
h8man <h8man@h8man.co>
2026-07-07 14:07:47 +0300

    tmp

--- Load & Lock.cpp
+++ Load & Lock.cpp
@@ -1,7 +1,24 @@
 #include "src/core/Application.h"
 
+namespace
+{
+    int RunApplication()
+    {
+        const core::Application application;
+        return application.Run();
+    }
+}
+
 int main()
 {
-    const core::Application application;
-    return application.Run();
+    return RunApplication();
 }
+
+#ifdef _WIN32
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    return RunApplication();
+}
+#endif
